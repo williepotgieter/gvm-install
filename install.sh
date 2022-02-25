903bf6e3562c874afbdc0d22f301a851670833b5
williepotgieter <willie@yellowtail.co.za>
2022-02-25 16:21:32 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# GVM Variables
+export PATH=$PATH:/usr/local/sbin \
+INSTALL_PREFIX=/usr/local \
+SOURCE_DIR=$HOME/source \
+BUILD_DIR=$HOME/build \
+INSTALL_DIR=$HOME/install \
+GVM_VERSION=21.4.4 \
+GVM_LIBS_VERSION=$GVM_VERSION
+
+source gvm.sh
